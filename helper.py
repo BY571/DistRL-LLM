cadcdd8ad12e32075a40dfad879851229a5c54b7
BY571 <Sebastian.dittert@gmx.de>
2025-02-27 16:53:42 +0100

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,9 @@
+
+
+r1_preprompt = (
+    "A conversation between User and Assistant. The user asks a question, and the Assistant solves it.\n"
+    "The assistant first thinks about the reasoning process and then provides the user with the answer.\n" 
+    "The response must follow this format:\n"
+    "<think> reasoning process here </think>\n"
+    "<answer> answer here </answer>\n"
+)
