deeba7c3ecfe3110353935796b0f19cfbeff9c4b
BY571 <Sebastian.dittert@gmx.de>
2025-02-27 16:54:42 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -7,3 +7,17 @@ r1_preprompt = (
     "<think> reasoning process here </think>\n"
     "<answer> answer here </answer>\n"
 )
+
+def process_dataset(tokenizer, dataset, preprompt, postprompt):
+    def generate_messages(examples):
+
+        messages = [[{"role": "system", "content": preprompt}, {"role": "user", "content": p+' '+ postprompt}] for p in examples["problem"]]
+        chat_messages = [tokenizer.apply_chat_template(
+                m,
+                add_generation_prompt=True,
+                tokenize=False,
+            ) for m in messages]
+
+        return {"problem": chat_messages}
+    
+    return dataset.map(generate_messages, batched=True)
