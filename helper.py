b8c5db20cc48f3f39aa9d5ff0e34909af8c60edb
BY571 <sebastian.dittert@gmx.de>
2025-03-12 09:22:16 +0100

    fix init peft

--- helper.py
+++ helper.py
@@ -21,3 +21,26 @@ def process_dataset(tokenizer, dataset, preprompt="", postprompt=""):
         return {"problem": chat_messages}
     
     return dataset.map(generate_messages, batched=True)
+
+def init_peft_model(base_model, lora_rank=32, lora_alpha=16):
+    return FastLanguageModel.get_peft_model(
+        base_model,
+        r=lora_rank,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        target_modules=[
+            "q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj",
+            "gate_proj",
+            "up_proj",
+            "down_proj",
+        ],
+        lora_alpha=lora_alpha,
+        lora_dropout=0,  # Supports any, but = 0 is optimized
+        bias="none",  # Supports any, but = "none" is optimized
+        # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+        use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+        random_state=3407,
+        use_rslora=False,  # We support rank stabilized LoRA
+        loftq_config=None,  # And LoftQ
+    )
